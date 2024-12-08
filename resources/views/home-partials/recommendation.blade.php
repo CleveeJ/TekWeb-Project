63d74ee08c0642fec9e3839d2efc0b371af0a335
CleveeJ <willsoncj285@gmail.com>
2024-12-08 23:03:55 +0700

    Feat randomize and cache recommend recipe based on date (change each day), change aspect ratio card to 6/9, set image to cover, and set recommendation recipe based on random data sent. Undone for desc

--- resources/views/home-partials/recommendation.blade.php
+++ resources/views/home-partials/recommendation.blade.php
@@ -6,7 +6,8 @@
                     0 0 30px rgba(255, 255, 255, 0.3);
         border: 2px solid rgba(255, 255, 255, 0.2);
         position: relative;
-        width: 24rem;
+        width: 21rem;
+        aspect-ratio: 6 / 9;
     }
     .container:hover {
         box-shadow: 0 0 15px rgba(255, 255, 255, 0.8), 
@@ -21,20 +22,21 @@
 </style>
 
 <section class="relative h-screen"  id="recommendation">
-    <img src="{{asset('assets/rempah_bg.jpeg')}}" alt="Background" class="absolute top-0 left-0 w-full h-screen object-cover">
+    <img src="{{asset('assets/rempah_bg.jpeg')}}" alt="Background" class="absolute top-0 left-0 w-full h-screen object-cover filter brightness-[60%]">
     <div class="flex flex-col gap-5 items-center justify-center h-full relative z-10 text-white">
         <div class="flex items-center justify-center z-10 font-bold text-5xl text-white">
             Recommendation Of The Day
         </div>
         <div class="container rounded-xl shadow-lg text-center">
-            <div>
-                <img src="{{asset('assets/burger.jpg')}}" alt="recommendation" class="rounded-lg">
+            <div class="w-full h-full overflow-hidden">
+                <img src="{{$recommend['image']}}" alt="recommendation" class="rounded-lg object-cover w-full h-full">
             </div>
             <div class="absolute w-98 bottom-0 left-0 right-0 p-4 bg-white bg-opacity-20 backdrop-blur-md text-white rounded-b-xl border border-white/30" id="bodyCard">
-                <div class="text-3xl font-bold text-left">Tittle</div>
-                <div class="text-sm text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, voluptatibus. Tempore debitis asperiores in tempora nam amet, commodi, adipisci reprehenderit odit corrupti, qui cum harum aspernatur ipsa. Totam, reprehenderit porro?</div>
+                <div class="text-3xl font-bold text-left">{{$recommend['name']}}</div>
+                <div class="text-md font-bold text-left">{{$recommend['user_name']}}</div>
+                <div class="text-sm text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, voluptatibus. Tempore debitis asperiores in tempora nam amet, commodi, adipisci reprehenderit odit corrupti</div>
                 <div class="buttons grid grid-cols-[repeat(2,_130px)] grid-rows-[40px] gap-[5px] mt-[20px]">
-                    <button class="border-[none] bg-[#eee] tracking-[3px] font-[Poppins] font-medium text-black">SEE MORE</button>
+                    <button onclick="openDetails(this)" data-href="{{ route('recipe.detail', ['recipe_id' => $recommend['id']]) }}" class="border-[none] bg-[#eee] tracking-[3px] font-[Poppins] font-medium text-black">SEE MORE</button>
                 </div>
             </div>
         </div>
@@ -73,4 +75,9 @@
     tlr.from('#bodyCard', {
         opacity: '0'
     }, 2);
+
+    function openDetails(button){
+        const url = button.getAttribute('data-href');
+        window.location.href = url;
+    }
 </script>
